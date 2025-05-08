5404ba110a4c3e87e768620bc4a92a1217b3512e
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-05-08 10:58:21 -0400

    Ya el modelo,servicios y contexto realizado

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/dao/TecnicoDao.kt
@@ -0,0 +1,27 @@
+package edu.ucne.registrotecnico.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TecnicoDao {
+    @Upsert
+    suspend fun save(Tecnico:TecnicoEntity)
+
+    @Query(
+        """Select*From Tecnicos
+            Where TecnicoId=:id
+            Limit 1
+        """
+    )
+    suspend fun  fin(id:Int):TecnicoEntity?
+    @Delete
+    suspend fun delete(Tecnico: TecnicoEntity)
+    @Query("Select*From Tecnicos")
+    fun getAll():Flow<List<TecnicoEntity>>
+
+}
