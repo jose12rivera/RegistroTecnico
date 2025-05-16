38e75432debf686b66707614ff3952e3e8e3adca
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-05-15 23:55:01 -0400

    solo me falta algo

--- app/src/main/java/edu/ucne/registrotecnico/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnico/MainActivity.kt
@@ -3,62 +3,38 @@ package edu.ucne.registrotecnico
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.viewmodel.compose.viewModel
-import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
-import edu.ucne.registrotecnico.presentacion.Tecnicos.TecnicoListScreen
-import edu.ucne.registrotecnico.presentacion.Tecnicos.TecnicoScreen
+import androidx.activity.enableEdgeToEdge
+import androidx.room.Room
+import edu.ucne.registrotecnico.data.local.database.TecnicoDb
+import edu.ucne.registrotecnico.data.respository.TecnicosRepository
 import edu.ucne.registrotecnico.presentacion.Tecnicos.TecnicoViewModel
+import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.registrotecnico.ui.theme.RegistroTecnicoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MainApp()
+        enableEdgeToEdge()
+
+        val tecnicoDb = Room.databaseBuilder(
+            applicationContext,
+            TecnicoDb::class.java,
+            "Tecnico.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+        val tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
+        val tecnicoViewModel = TecnicoViewModel(tecnicosRepository)
+
+        setContent() {
+            RegistroTecnicoTheme {
+                val navController = rememberNavController()
+                TecnicosNavHost(
+                    navHostController = navController,
+                    viewModel = tecnicoViewModel
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun MainApp(tecnicoViewModel: TecnicoViewModel = viewModel()) {
-    var showForm by remember { mutableStateOf(false) }
-    var tecnicoToEdit by remember { mutableStateOf<TecnicoEntity?>(null) }
-
-    if (showForm) {
-        TecnicoScreen(
-            tecnico = tecnicoToEdit,
-            onSave = { nombre, sueldo ->
-                if (tecnicoToEdit == null) {
-                    tecnicoViewModel.addTecnico(nombre, sueldo)
-                } else {
-                    tecnicoViewModel.updateTecnico(
-                        tecnicoToEdit!!.copy(Nombre = nombre, Sueldo = sueldo)
-                    )
-                }
-                showForm = false
-                tecnicoToEdit = null
-            },
-            onCancel = {
-                showForm = false
-                tecnicoToEdit = null
-            }
-        )
-    } else {
-        TecnicoListScreen(
-            tecnicoList = tecnicoViewModel.tecnicoList,
-            onCreate = {
-                tecnicoToEdit = null
-                showForm = true
-            },
-            onDelete = { tecnicoViewModel.deleteTecnico(it) },
-            onEdit = {
-                tecnicoToEdit = it
-                showForm = true
-            }
-        )
     }
 }
