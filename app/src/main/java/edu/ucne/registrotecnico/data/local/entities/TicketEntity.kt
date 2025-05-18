3d5edb836db06fe51340c868960913b3425c1d09
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-05-18 01:11:33 -0400

    El Ticket entity

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/entities/TicketEntity.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registrotecnico.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tickets")
+data class TicketEntity(
+    @PrimaryKey(autoGenerate = true) val TicketId: Int = 0,
+    val Fecha: String,
+    val Cliente: String,
+    val Asunto: String,
+    val Descripcion: String,
+    val Prioridad: String // <--- Este campo debe estar
+)
+
