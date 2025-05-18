fb6f6054675bf72b00df4ea03dde05003f7cba3b
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-05-18 01:09:04 -0400

    main activity

--- app/src/main/java/edu/ucne/registrotecnico/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnico/MainActivity.kt
@@ -4,35 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import edu.ucne.registrotecnico.data.local.database.TecnicoDb
 import edu.ucne.registrotecnico.data.respository.TecnicosRepository
+import edu.ucne.registrotecnico.data.respository.TicketRepository
 import edu.ucne.registrotecnico.presentacion.Tecnicos.TecnicoViewModel
-import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
-import androidx.navigation.compose.rememberNavController
+import edu.ucne.registrotecnico.presentacion.Tickets.TicketViewModel
 import edu.ucne.registrotecnico.ui.theme.RegistroTecnicoTheme
+import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
 
 class MainActivity : ComponentActivity() {
+    private lateinit var tecnicoDb: TecnicoDb
+    private lateinit var tecnicosRepository: TecnicosRepository
+    private lateinit var tecnicosViewModel: TecnicoViewModel
+    private lateinit var ticketRepository: TicketRepository
+    private lateinit var ticketViewModel: TicketViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val tecnicoDb = Room.databaseBuilder(
-                applicationContext,
-                TecnicoDb::class.java,
-                "Tecnico.db"
-            ).fallbackToDestructiveMigration(false)
+        val database = Room.databaseBuilder(
+            applicationContext,
+            TecnicoDb::class.java,
+            "Tecnico.db"
+        ).fallbackToDestructiveMigration()
             .build()
 
-        val tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
+        val tecnicosRepository = TecnicosRepository(database.TecnicoDao())
         val tecnicoViewModel = TecnicoViewModel(tecnicosRepository)
 
-        setContent() {
+        val ticketRepository = TicketRepository(database.TicketDao())
+        val ticketViewModel = TicketViewModel(ticketRepository)
+
+        setContent {
             RegistroTecnicoTheme {
                 val navController = rememberNavController()
                 TecnicosNavHost(
                     navHostController = navController,
-                    viewModel = tecnicoViewModel
+                    tecnicoViewModel = tecnicoViewModel,
+                    ticketViewModel = ticketViewModel
                 )
             }
         }
