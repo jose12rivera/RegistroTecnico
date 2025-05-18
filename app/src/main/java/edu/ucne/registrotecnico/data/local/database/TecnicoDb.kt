6366e7c07e3ed3eef7ac8039c188949b9f477cb0
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-05-18 01:09:46 -0400

    la base de datos

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -0,0 +1,21 @@
+package edu.ucne.registrotecnico.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
+import edu.ucne.registrotecnico.data.local.dao.TicketDao
+import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnico.data.local.entities.TicketEntity
+
+@Database(
+    entities = [
+        TecnicoEntity::class,
+        TicketEntity::class
+    ],
+    version = 3,
+    exportSchema = false
+)
+abstract class TecnicoDb : RoomDatabase() {
+    abstract fun TecnicoDao(): TecnicoDao
+    abstract fun TicketDao(): TicketDao
+}
